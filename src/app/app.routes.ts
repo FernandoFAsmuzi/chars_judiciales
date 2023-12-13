af393891807a322937318fb630fa9f92cc2c3385
FernandoAsmuzi <fernandoasmuzi94@gmail.com>
2023-12-13 12:57:59 -0300

    add: componentes dashboard, header, login y logo nuevo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 import { ChartsComponent } from './charts/charts.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: ChartsComponent },	
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+    { path: 'login', component: LoginComponent},
+
+    { path: 'dashboard', 
+        component: DashboardComponent,
+        children: [
+            { path: 'charts', component: ChartsComponent }
+        ]
+    },	
 ];
